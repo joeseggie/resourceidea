e8415aeeeb78be25ad57c77e8465a6e92ba0f908
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-25 13:53:22 +0300

    PaymentMethod model.

---
+++ app/models/payment_method.py
@@ -0,0 +1,19 @@
+"""Payment method model.
+"""
+from app import db
+
+
+class PaymentMethod(db.Model):
+    """Payment method model.
+
+    Parameters
+    ----------
+        db : Model
+    """
+    __tablename__ = 'payment_method'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Payment method %s>' % self.description
