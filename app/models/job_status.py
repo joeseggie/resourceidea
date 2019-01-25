ec5270f88ed18441be1d0a3a95af4abef2028e6f
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-25 13:52:16 +0300

    JobStatus model.

---
+++ app/models/job_status.py
@@ -0,0 +1,19 @@
+"""Job status model.
+"""
+from app import db
+
+
+class JobStatus(db.Model):
+    """Job status model
+
+    Parameters
+    ----------
+    db : Model
+    """
+    __tablename__ = 'job_status'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Job status %s>' % self.description
