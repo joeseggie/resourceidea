78f6c3414dbaaa9247f2de885b2104c6f9b5746a
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-25 15:13:50 +0300

    Job position model.

---
+++ app/models/job_position.py
@@ -0,0 +1,29 @@
+"""Job position model.
+"""
+from app import db
+
+
+class JobPosition(db.Model):
+    """Job position model.
+
+    Parameters
+    ----------
+    db : Model
+    """
+    __tablename__ = 'job_position'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    level = db.Column(db.Integer)
+
+    department_id = db.Column(
+        db.Integer,
+        db.ForeignKey('department.id')
+    )
+    department = db.relationship(
+        'Department',
+        backref=db.backref(
+            'job_positions',
+            lazy=True
+        )
+    )
