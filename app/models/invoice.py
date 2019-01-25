f17055e0e811fcfe6d0607f0839c8636fa038baf
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-25 14:57:15 +0300

    Invoice model

---
+++ app/models/invoice.py
@@ -0,0 +1,31 @@
+"""Invoice model.
+"""
+from app import db
+
+
+class Invoice(db.Model):
+    """Invoice model.
+
+    Parameters
+    ----------
+    db : Model
+    """
+    __tablename__ = 'invoice'
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float)
+    invoice_date = db.Column(db.DateTime)
+    invoice_period_start = db.Column(db.DateTime)
+    invoice_period_end = db.Column(db.DateTime)
+
+    subscription_id = db.Column(
+        db.Integer,
+        db.ForeignKey('subscription.id')
+    )
+    subscription = db.relationship(
+        'Subscription',
+        backref=db.backref(
+            'invoices',
+            lazy=True
+        )
+    )
