b5355c1d1823314fa0ecf531505a42f2302c5a07
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-26 02:00:08 +0300

    Changing application setup.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,48 @@
-"""App initializer.
+"""Application initializer.
 """
-from app.resourceidea import app, db
-from app.models.assignment_status import AssignmentStatus
+import os
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+CONFIG_ENV = {
+    'default': 'config.default.Config',
+    'development': 'config.development.DevelopmentConfig',
+    'production': 'config.production.ProductionConfig',
+    'testing': 'config.testing.TestingConfig',
+}
+
+
+def setup_app(app: Flask):
+    """Setup the application.
+
+    Parameters
+    ----------
+    app {Flask}
+        Flask application to be setup.
+    """
+    config_name = os.getenv('FLASK_ENV', 'default')
+    app.config.from_object(CONFIG_ENV[config_name])
+
+
+app = Flask('resourceidea')
+setup_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app.routes.api.index import index_blueprint
+from app.routes.api.company import company_blueprint
+
+
+app.register_blueprint(
+    index_blueprint,
+    url_prefix='/api/v1.0'
+)
+
+app.register_blueprint(
+    company_blueprint,
+    url_prefix='/api/v1.0/companies'
+)
