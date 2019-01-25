2b0d32be900a93da9c3c7d040873c8d08eb83b85
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-25 13:48:28 +0300

    Disabling tracking SQLAlchemy Track modifications.

--- config/default.py
+++ config/default.py
@@ -8,5 +8,6 @@ class Config:
     """
     APP_NAME = 'resourceidea'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
