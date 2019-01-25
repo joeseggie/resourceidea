bb0104cbe0d5751e443b74c783244014463083a2
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-25 13:52:07 +0300

    Company model.

---
+++ app/models/company.py
@@ -0,0 +1,21 @@
+"""Company model
+"""
+from app import db
+
+
+class Company(db.Model):
+    """Company model.
+
+    Parameters
+    ----------
+    db : Model
+    """
+    __tablename__ = 'company'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    name_stub = db.Column(db.String(256))
+    address = db.Column(db.String(256))
+
+    def __repr__(self):
+        return '<Company %s>' % self.name
