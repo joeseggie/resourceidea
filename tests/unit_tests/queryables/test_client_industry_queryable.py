b8e899abeed57e8e344d85fdf9da708e931cd75b
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-28 13:56:56 +0300

    Client industry queryable. Closes #4

---
+++ tests/unit_tests/queryables/test_client_industry_queryable.py
@@ -0,0 +1,23 @@
+"""Test client industry queryable.
+"""
+from unittest.mock import Mock
+
+from app.queryables.client_industry_queryable import ClientIndustryQueryable
+
+
+def test_to_list_returns_list():
+    """Tests if to_list method returns a list.
+    """
+    queryable = ClientIndustryQueryable()
+    result = queryable.to_list()
+    assert isinstance(result, list)
+
+
+def test_where_company_is_returns_Query():
+    """Test if the where_company_is method returns
+    a ClientIndustryQueryable object.
+    """
+    queryable = ClientIndustryQueryable()
+    company_id = Mock()
+    result = queryable.where_company_is(company_id)
+    assert isinstance(result, ClientIndustryQueryable)
