0b70c30282c30be6142279a2303eb049d19cc712
Joseph Serunjogi <joeseggie@gmail.com>
2019-01-28 17:53:55 +0300

    Subscription queryable. Closes #1.

---
+++ app/queryables/subscription_queryable.py
@@ -0,0 +1,36 @@
+'''Subscription queryable.
+'''
+from app.models.subscription import Subscription
+
+
+class SubscriptionQueryable:
+    '''Subscription queryable.
+    '''
+    def __init__(self):
+        '''Constructor.
+        '''
+        self.subscription_queryable = Subscription.query
+
+    def to_list(self):
+        '''List subscriptions.
+        '''
+        query_result = self.subscription_queryable.all()
+        subscriptions_list = [{
+            'Id': result.id,
+            'Started': result.started,
+            'Ended': result.ended,
+            'CompanyId': result.company_id
+        } for result in query_result]
+        return subscriptions_list
+
+    def where_company_is(self, company_id: int):
+        '''Filter subscriptions by company.
+
+        Parameters
+        ----------
+        company_id {int} -- Company Id.
+        '''
+        self.subscription_queryable = self.subscription_queryable.filter_by(
+            company_id=company_id
+        )
+        return self
