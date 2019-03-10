5ad6b78928e01319de756cea1bd850ced38db322
Joseph Serunjogi <joeseggie@gmail.com>
2019-03-10 21:45:34 +0300

    Testing using pytest

--- tests/integration/test_service_plan_routes.py
+++ tests/integration/test_service_plan_routes.py
@@ -1,25 +1,41 @@
-from flask import current_app
-import unittest
+import json
+
+import pytest
 
 from app import create_app, db
 
 
-class BasicTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app('testing')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        db.init_app(self.app)
-        print(self.app.config)
+@pytest.fixture
+def client():
+    app = create_app('testing')
+    client = app.test_client()
+    with app.app_context() as app_context:
+        app_context.push()
+        db.init_app(app)
+        print(app.config)
         db.create_all()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
+    yield client
 
-    def test_app_exists(self):
-        self.assertFalse(current_app is None)
+    db.session.remove()
+    db.drop_all()
+    app_context.pop()
 
-    def test_app_is_testing(self):
-        self.assertTrue(current_app.config['TESTING'])
+
+class TestServicePlanRoutes:
+    '''
+    ServicePlans test suite.
+    '''
+
+    def test_base_route(self, client):
+        '''
+        Test the base route response.
+        '''
+
+        # Act
+        rv = client.get('/api/v1/serviceplans')
+        data = json.loads(rv.get_data(as_text=True))
+
+        # Assert
+        assert rv.status_code == 200
+        assert data['status'] == 'OK'
