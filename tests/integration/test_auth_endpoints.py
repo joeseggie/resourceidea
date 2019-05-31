8ef01458d7ac8c89b42b8e60f9cd3d0fa7f1dc66
Joseph Serunjogi <joeseggie@gmail.com>
2019-06-01 00:56:51 +0300

    Fixes AB#35, AB#34, AB#36 unique user fields

--- tests/integration/test_auth_endpoints.py
+++ tests/integration/test_auth_endpoints.py
@@ -50,3 +50,68 @@ def test_signup_organization_exists(app, session):
     assert client_response.status_code == 400
     assert 'status' in output
     assert output['status'] == 'Organization name already exists'
+
+
+def test_user_signup_email_exists(app, session):
+    # Arrange
+    client = app.test_client()
+    request_body = {
+        'organization': 'Organization 12',
+        'username': 'test123',
+        'password': 'strong_password',
+        'confirm_password': 'strong_password',
+        'email': 'mail@example.com'
+    }
+
+    # Act
+    result = client.post('/api/v0.1/signup', json=request_body)
+    output = json.loads(result.get_data(as_text=True))
+
+    # Assert
+    assert result.status_code == 400
+    assert 'status' in output
+    assert output['status'] == 'Email already exists'
+
+
+def test_user_signup_phone_number_exists(app, session):
+    # Arrange
+    client = app.test_client()
+    request_body = {
+        'organization': 'Organization 12',
+        'username': 'test_user',
+        'password': 'strong_password',
+        'confirm_password': 'strong_password',
+        'phone_number': '000-0000-111',
+        'email': 'e1.mail@example.com'
+    }
+
+    # Act
+    result = client.post('/api/v0.1/signup', json=request_body)
+    output = json.loads(result.get_data(as_text=True))
+
+    # Assert
+    assert result.status_code == 400
+    assert 'status' in output
+    assert output['status'] == 'Phone number already exists'
+
+
+def test_user_signup_username_exists(app, session):
+    # Arrange
+    client = app.test_client()
+    request_body = {
+        'organization': 'Organization 12',
+        'username': 'test_user',
+        'password': 'strong_password',
+        'confirm_password': 'strong_password',
+        'phone_number': '000-0010-111',
+        'email': 'e1.mail@example.com'
+    }
+
+    # Act
+    result = client.post('/api/v0.1/signup', json=request_body)
+    output = json.loads(result.get_data(as_text=True))
+
+    # Assert
+    assert result.status_code == 400
+    assert 'status' in output
+    assert output['status'] == 'Username is already taken'
