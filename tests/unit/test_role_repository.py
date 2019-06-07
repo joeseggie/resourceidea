01e15dea17cdd57ab622c56e3a42bcd81c4d7fe0
Joseph Serunjogi <joeseggie@gmail.com>
2019-06-07 09:37:50 +0300

    Role repository tests update
    Updating the role repository tests to cater for
    CI testing.

--- tests/unit/test_role_repository.py
+++ tests/unit/test_role_repository.py
@@ -7,19 +7,17 @@ def test_update(session):
     Test the role repository update function.
     """
     # Arrange
-    test_model_id = RoleRepository.get_all(
-        sort_key='name',
-        sort_order='asc'
-    )[0].id
+    test_model = RoleRepository.create(Role(name='Super User'))
+    test_model_id = test_model.id
     update_fields = ('name',)
 
     # Act
-    result = RoleRepository.update(test_model_id, update_fields, name='Super User')
+    result = RoleRepository.update(test_model_id, update_fields, name='Admin User')
 
     # Assert
     assert isinstance(result, Role)
-    assert result.normalized_name == 'super-user'
-    assert result.name == 'Super User'
+    assert result.normalized_name == 'admin-user'
+    assert result.name == 'Admin User'
 
 
 def test_get_all(session):
@@ -42,11 +40,32 @@ def test_get_by_name(session):
     Test role repository get_by_name function.
     """
     # Arrange
-    role_name = 'Administrator'
-    normalized_role_name = 'administrator'
+    role_name = 'Super User'
+    normalized_role_name = 'super-user'
+    RoleRepository.create(
+        Role(name=role_name, normalized_name=normalized_role_name))
+
 
     # Act
     result = RoleRepository.get_by_name(role_name)
+
+    # Assert
+    assert isinstance(result, Role)
+    assert result.name == role_name
+    assert result.normalized_name == normalized_role_name
+
+
+def test_create(session):
+    """
+    Test role repository create function.
+    """
+    # Arrange
+    role_name = 'Super User'
+    normalized_role_name = 'super-user'
+
+    # Act
+    result = RoleRepository.create(
+        Role(name=role_name, normalized_name=normalized_role_name))
 
     # Assert
     assert isinstance(result, Role)
