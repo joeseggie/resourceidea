97dd54b27a2e15979000aecd50631a54088a5f26
Joseph Serunjogi <joeseggie@gmail.com>
2019-06-21 00:14:08 +0300

    Fixes AB#57
    Update job details.

--- app/job/utils.py
+++ app/job/utils.py
@@ -1,4 +1,6 @@
 """app.job.utils module"""
+from uuid import UUID
+
 from app.job.models import Job
 from app.job.repositories import JobRepository
 
@@ -12,3 +14,28 @@ def create_job(**kwargs) -> Job:
     """
     new_job = Job(**kwargs)
     return JobRepository.create(new_job)
+
+
+def update_job(job_id: UUID, **kwargs) -> Job:
+    """
+    Update job.
+
+    Args:
+        job_id (UUID): ID of job to be update.
+
+    Returns:
+        Updated job.
+
+    Raises:
+        ValueError if the job with the ID does not exist.
+    """
+    job_for_update = JobRepository.get_one_by_id(job_id)
+    if not job_for_update:
+        raise ValueError('Job with ID specified does not exists')
+
+    update_fields = ('title', 'description', 'start_date', 'completion_date',
+                     'status')
+    return JobRepository.update_by_id(
+        model_id=job_for_update.id,
+        fields_for_update=update_fields,
+        **kwargs)
--- tests/unit/test_job_utils.py
+++ tests/unit/test_job_utils.py
@@ -1,7 +1,10 @@
 """Tests for app.jobs.utils module"""
+from datetime import date
+
 from app.engagement.utils import list_engagements
 from app.job.models import Job
 from app.job.utils import create_job
+from app.job.utils import update_job
 
 
 def test_create_job(session, fake_lorem):
@@ -20,4 +23,36 @@ def test_create_job(session, fake_lorem):
 
     # Assert
     if not isinstance(result, Job):
+        raise AssertionError()
+
+
+def test_update_job(session, fake_lorem):
+    """Test update_job function"""
+
+    # Arrange
+    fake_engagement_id = next(iter(list_engagements() or []), None).id
+    fake_job = {
+        'title': fake_lorem.sentence(),
+        'description': fake_lorem.paragraph(),
+        'engagement_id': fake_engagement_id
+    }
+    fake_job = create_job(**fake_job)
+    old_id = fake_job.id
+    old_title = fake_job.title
+    updates = {
+        'title': 'Changed title',
+        'description': 'Changed description',
+        'start_date': date.today(),
+        'completion_date': date.today()
+    }
+
+    # Act
+    result = update_job(fake_job.id, **updates)
+
+    # Assert
+    if not isinstance(result, Job):
+        raise AssertionError()
+    if old_id != result.id:
+        raise AssertionError()
+    if old_title == result.title:
         raise AssertionError()
