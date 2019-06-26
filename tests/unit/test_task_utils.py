02a9b3fa2da073736864e732b0f1eec225f63d8c
Joseph Serunjogi <joeseggie@gmail.com>
2019-06-26 15:04:19 +0300

    Fixes AB#93
    Update task details.

--- tests/unit/test_task_utils.py
+++ tests/unit/test_task_utils.py
@@ -2,6 +2,7 @@
 from app.job.utils import list_jobs
 from app.task.models import Task
 from app.task.utils import create_task
+from app.task.utils import update_task
 
 
 def test_create_task(session, fake_lorem):
@@ -20,4 +21,26 @@ def test_create_task(session, fake_lorem):
 
     # Assert
     if not isinstance(result, Task):
+        raise AssertionError()
+
+
+def test_update_task(session, fake_lorem):
+    """Test update_task function"""
+
+    # Arrange
+    fake_job_id = next(iter(list_jobs() or []), None).id
+    fake_task_data = {
+        'title': fake_lorem.sentence(),
+        'description': fake_lorem.paragraph(),
+        'job_id': fake_job_id
+    }
+    fake_task = create_task(**fake_task_data)
+    fake_task_title = fake_task.title
+    task_updates = {'title': 'Changed task'}
+
+    # Act
+    result = update_task(fake_task.id, **task_updates)
+
+    # Assert
+    if fake_task_title == result.title:
         raise AssertionError()
